b1bf47c571e68f8a0d8e61af50f9725562f1444c
Anshul <anshul41171@gmail.com>
2026-07-22 18:30:53 +0530

    Fix the signIn button

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,19 +6,26 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname + "/apps/web",
 });
 
 const eslintConfig = [
   {
     ignores: [
-      ".next/**",
-      "node_modules/**",
-      "dist/**",
-      "build/**",
-      "tsconfig.tsbuildinfo",
-      "next-env.d.ts",
+      "**/node_modules/**",
+      "**/.next/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/tsconfig.tsbuildinfo",
+      "**/next-env.d.ts",
     ],
+  },
+  {
+    settings: {
+      next: {
+        rootDir: "apps/web",
+      },
+    },
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
